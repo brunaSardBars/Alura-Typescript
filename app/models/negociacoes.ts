0a8fe026596146309dc60249c7f3c83a7e3fa71d
brunasa <brunasa@synnex.com>
2022-08-03 17:35:04 -0300

    melhorias

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,12 +1,14 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
+    //private negociacoes: Array<Negociacao> = [];
     private negociacoes: Negociacao[] = [];   
 
     adiciona(negociacao: Negociacao): void {
         this.negociacoes.push(negociacao);
     }
 
+    // lista(): readonly Negociacao[] {
     lista(): ReadonlyArray<Negociacao> {
         return this.negociacoes;
     }
