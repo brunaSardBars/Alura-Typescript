f95c58e367f4328768804d0d32cc01034e5489bd
brunasa <brunasa@synnex.com>
2022-08-17 18:33:40 -0300

    decorators

---
+++ app/dist/js/decorators/inspect.js
@@ -0,0 +1,11 @@
+export function inspect(target, propertyKey, descriptor) {
+    const metodoOriginal = descriptor.value;
+    descriptor.value = function (...args) {
+        console.log(`--- Método ${propertyKey} ---`);
+        console.log(`--- Parameters ${JSON.stringify(args)} ---`);
+        const retorno = metodoOriginal.apply(this, args);
+        console.log(`--- Return ${JSON.stringify(retorno)} ---`);
+        return retorno;
+    };
+    return descriptor;
+}
--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,26 +1,25 @@
+import { inspect } from "../decorators/inspect.js";
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
+
 export abstract class View<T> {
     protected elemento: HTMLElement;
-    private escapar = false;
 
-    constructor(seletor: string, escapar?: boolean) {
+    constructor(seletor: string) {
         const elemento = document.querySelector(seletor);
         if (elemento) {
             this.elemento = <HTMLElement>elemento;
         } else {
             throw Error(`Seletor ${seletor} não existe no DOM`);
-        }
-        if(escapar) {
-            this.escapar = escapar;
-        }
+        }      
     }
 
     protected abstract template(model: T): string;
 
-    public update(model: T): void {
+    //decorators
+    // @logarTempoDeExecucao(true)    
+    // @inspect    
+    public update(model: T): void {        
         let template = this.template(model);
-        if (this.escapar) {
-            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
-        }
-        this.elemento.innerHTML = template;
+        this.elemento.innerHTML = template;        
     }
 }
